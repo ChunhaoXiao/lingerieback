38633f1e80947de07306b2603543867765a87179
Your Name <you@example.com>
2024-11-24 10:53:23 +0800

    docker-compose 更新

--- apis/v1/route_post.py
+++ apis/v1/route_post.py
@@ -84,6 +84,7 @@ def post_detail(id:int,  background_tasks: BackgroundTasks, db:Session = Depends
     #datas = post.__dict__
     #PostShow.model_validate(post)
     response = PostShow(**post.__dict__)
+    print(f"response===================>{response}")
     response.is_admin = current_user.is_admin
     
     return {"code":1,"data":response}
